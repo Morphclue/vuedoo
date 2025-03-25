74d7397cda3bdbafd19ffa0715385f5c990606c4
Julian Holfeld <morphclue@gmail.com>
2025-03-25 18:33:50 +0100

    feat: align Tasks with Input

--- apps/frontend/src/views/HomeView.vue
+++ apps/frontend/src/views/HomeView.vue
@@ -18,11 +18,14 @@ const addTask = () => {
 </script>
 
 <template>
-  <v-container>
-    <v-container class="d-flex flex-1-0">
-      <v-text-field v-model="newTask" placeholder="New Task..."/>
+  <v-container class="d-flex flex-column gap-4">
+    <div class="d-flex gap-2">
+      <v-text-field v-model="newTask" placeholder="New Task..." />
       <v-btn @click="addTask">Add Task</v-btn>
-    </v-container>
-    <Task v-for="task in tasks" :title="task.title" :done="task.done"></Task>
+    </div>
+
+    <div class="d-flex flex-column gap-2">
+      <Task v-for="task in tasks" :key="task.title" :title="task.title" :done="task.done" />
+    </div>
   </v-container>
 </template>
