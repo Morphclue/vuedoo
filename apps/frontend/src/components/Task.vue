af2a5674ee0783e062a745a70277f235cefe9fe5
Julian Holfeld <morphclue@gmail.com>
2025-03-25 18:46:18 +0100

    style: Task

--- apps/frontend/src/components/Task.vue
+++ apps/frontend/src/components/Task.vue
@@ -1,17 +1,25 @@
 <script setup lang="ts">
 import {ref} from 'vue';
+import {useTheme} from 'vuetify';
 
 const props = defineProps<{title: string, done: boolean}>();
 const isChecked = ref(props.done)
+const theme = useTheme();
 </script>
 
 <template>
-  <v-row align="center">
-    <v-checkbox v-model="isChecked"></v-checkbox>
-    <h3 class="text-h5" :class="{ 'text-decoration-line-through text-gray-500': isChecked }">
-      {{ title }}
-    </h3>
-  </v-row>
+  <v-hover v-slot="{ isHovering, props }">
+    <div
+      v-bind="props"
+      class="d-flex align-center mb-2 rounded"
+      :style="{
+        backgroundColor: isHovering ? theme.current.value.colors.accent : theme.current.value.colors.primary,
+      }"
+    >
+      <div class="d-flex align-center">
+        <v-checkbox v-model="isChecked" />
+        <p class="mb-3" :class="{ 'text-decoration-line-through text-grey': isChecked }">{{ title }}</p>
+      </div>
+    </div>
+  </v-hover>
 </template>
-
-<style scoped></style>
--- apps/frontend/src/plugins/vuetify.ts
+++ apps/frontend/src/plugins/vuetify.ts
@@ -10,6 +10,7 @@ export const vuetify = createVuetify({
         colors: {
           primary: '#255F38',
           secondary: '#18230F',
+          accent: '#1F7D53',
           error: '#FF5252',
           info: '#2196F3',
           success: '#4CAF50',
