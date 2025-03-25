a06479816ce578f7c537c73ba11768dcf67282d8
Julian Holfeld <morphclue@gmail.com>
2025-03-25 19:42:30 +0100

    feat: Eisenhower Matrix content

--- apps/frontend/src/views/EisenhowerView.vue
+++ apps/frontend/src/views/EisenhowerView.vue
@@ -1,7 +1,34 @@
-<script lang="ts">
-</script>
-
 <template>
-
+  <v-container fluid class="pa-0 body-height">
+    <v-row no-gutters style="height: 50%;">
+      <v-col cols="6" class="pa-1">
+        <v-card class="pa-4 fill-height" color="red lighten-4" variant="outlined">
+          <h3 class="text-h6 font-weight-bold mb-2">Important & Urgent</h3>
+        </v-card>
+      </v-col>
+      <v-col cols="6" class="pa-1">
+        <v-card class="pa-4 fill-height" color="orange lighten-4" variant="outlined">
+          <h3 class="text-h6 font-weight-bold mb-2">Important & Not urgent</h3>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row no-gutters style="height: 50%;">
+      <v-col cols="6" class="pa-1">
+        <v-card class="pa-4 fill-height" color="blue lighten-4" variant="outlined">
+          <h3 class="text-h6 font-weight-bold mb-2">Not important & Urgent</h3>
+        </v-card>
+      </v-col>
+      <v-col cols="6" class="pa-1">
+        <v-card class="pa-4 fill-height" color="grey lighten-4" variant="outlined">
+          <h3 class="text-h6 font-weight-bold mb-2">Not important & Not urgent</h3>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
+<style scoped>
+.body-height {
+  height: calc(100vh - 64px);
+}
+</style>
