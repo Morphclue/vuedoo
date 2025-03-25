cf43908113eac40025047b14111078e297c73edc
Julian Holfeld <morphclue@gmail.com>
2025-03-25 18:21:48 +0100

    feat: color palette

--- apps/frontend/src/plugins/vuetify.ts
+++ apps/frontend/src/plugins/vuetify.ts
@@ -1,7 +1,22 @@
 import '@mdi/font/css/materialdesignicons.css';
 import 'vuetify/styles';
-import { createVuetify } from 'vuetify';
+import {createVuetify} from 'vuetify';
 
 export const vuetify = createVuetify({
-  theme: { defaultTheme: 'light' },
+  theme: {
+    themes: {
+      vuedoo: {
+        dark: true,
+        colors: {
+          primary: '#255F38',
+          secondary: '#18230F',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FFC107',
+        },
+      },
+    },
+    defaultTheme: 'vuedoo',
+  },
 });
