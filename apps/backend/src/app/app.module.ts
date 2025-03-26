720437c7caa6ead84e62bc8f11f440f4fdf79ccd
Julian Holfeld <morphclue@gmail.com>
2025-03-26 19:34:09 +0100

    feat: mongoose

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { environment } from './environment';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(environment.mongo.uri)],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/backend/src/app/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  mongo: {
+    uri: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/nest'
+  }
+}
