a7e73ae073ff5dd6c7b60aea57abfc91bdc2f025
Julian Holfeld <morphclue@gmail.com>
2025-03-29 14:04:32 +0100

    fix: task in defineProps

--- apps/frontend/src/components/Task.vue
+++ apps/frontend/src/components/Task.vue
@@ -1,26 +1,23 @@
 <script setup lang="ts">
 import {ref} from 'vue';
 import {useTheme} from 'vuetify';
-import {TaskDto} from '@vuedoo/types';
 
-const props = defineProps<TaskDto>();
+const props = defineProps<{ title: string, completed: boolean }>();
 const isChecked = ref(props.completed)
 const theme = useTheme();
 </script>
 
 <template>
-  <v-hover v-slot="{ isHovering, props }">
+  <v-hover v-slot:default="hoverData">
     <div
-      v-bind="props"
+      v-bind="hoverData.props"
       class="d-flex align-center mb-2 rounded"
       :style="{
-        backgroundColor: isHovering ? theme.current.value.colors.accent : theme.current.value.colors.primary,
+        backgroundColor: hoverData.isHovering ? theme.current.value.colors.accent : theme.current.value.colors.primary,
       }"
     >
-      <div class="d-flex align-center">
         <v-checkbox v-model="isChecked" />
         <p class="mb-3" :class="{ 'text-decoration-line-through text-grey': isChecked }">{{ title }}</p>
-      </div>
     </div>
   </v-hover>
 </template>
--- libs/types/src/lib/dto/task.dto.ts
+++ libs/types/src/lib/dto/task.dto.ts
@@ -1,3 +1,5 @@
+import {OmitType} from '@nestjs/swagger';
 import {Task} from '../schema';
 
-export type TaskDto = Omit<Task, '_id' | 'id'>;
+// TODO: add id back if needed
+export class TaskDto extends OmitType(Task, ['_id', 'id'] as const) {}
