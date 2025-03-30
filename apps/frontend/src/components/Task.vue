85796d92f3b9d5f9cd7c60b8cea77c05c74e8b59
Julian Holfeld <morphclue@gmail.com>
2025-03-30 14:43:52 +0200

    feat: v-menu for deletion

--- apps/frontend/src/components/Task.vue
+++ apps/frontend/src/components/Task.vue
@@ -5,6 +5,11 @@ import {useTheme} from 'vuetify';
 const props = defineProps<{ title: string, completed: boolean }>();
 const isChecked = ref(props.completed)
 const theme = useTheme();
+
+const menuItems = [
+  { title: 'TODO'},
+  { title: 'Delete'}
+]
 </script>
 
 <template>
@@ -16,8 +21,24 @@ const theme = useTheme();
         backgroundColor: hoverData.isHovering ? theme.current.value.colors.accent : theme.current.value.colors.primary,
       }"
     >
-      <v-checkbox v-model="isChecked" />
-       <p class="mb-3" :class="{ 'text-decoration-line-through text-grey': isChecked }">{{ title }}</p>
+      <div class="d-flex justify-space-between w-100 align-center">
+        <div class="d-flex align-center">
+          <v-checkbox v-model="isChecked" />
+          <p class="mb-3" :class="{ 'text-decoration-line-through text-grey': isChecked }">{{ title }}</p>
+        </div>
+        <v-menu>
+          <template #activator="{ props: menuProps }">
+            <v-btn class="mr-2" v-bind="menuProps" icon>
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="item in menuItems" :key="item.title">
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
     </div>
   </v-hover>
 </template>
