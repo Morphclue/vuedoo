ff16c19e608228c3c0babfd5568344bfb6aecddd
Julian Holfeld <morphclue@gmail.com>
2025-04-26 17:59:32 +0200

    feat: use label for checkbox

--- apps/frontend/src/components/Task.vue
+++ apps/frontend/src/components/Task.vue
@@ -38,15 +38,11 @@ const emit = defineEmits<{
       }"
     >
       <div class="d-flex justify-space-between w-100 align-center">
-        <div class="d-flex align-center">
-          <v-checkbox v-model="isChecked" />
-          <p
-            class="mb-3"
-            :class="{ 'text-decoration-line-through text-grey': isChecked }"
-          >
-            {{ title }}
-          </p>
-        </div>
+        <v-checkbox
+          v-model="isChecked"
+          :label="title"
+          :class="{ 'text-decoration-line-through text-grey': isChecked }"
+        />
         <v-menu>
           <template #activator="{ props: menuProps }">
             <v-btn
